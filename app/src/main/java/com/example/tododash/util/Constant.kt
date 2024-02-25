9f79142596990b15aa7f1787eff14f8cf9e93ae0
AMiR <amiratashanii@gmail.com>
2024-02-26 01:04:06 +0330

    Init project

---
+++ app/src/main/java/com/example/tododash/util/Constant.kt
@@ -0,0 +1,6 @@
+package com.example.tododash.util
+
+object Constants {
+    const val TODO_TABLE = "todo_table"
+    const val DATABASE_NAME = "todo_dash_database"
+}
---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+plugins {
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.kotlinAndroid) apply false
+    alias(libs.plugins.kapt) apply false
+    alias(libs.plugins.hilt) apply false
+
+}
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+
+        // Android Build Server
+        maven { url = uri("../nowinandroid-prebuilts/m2repository") }
+    }
+
+}
